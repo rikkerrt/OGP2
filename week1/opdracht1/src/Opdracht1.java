30f96cc33f1e1b6f0966fc4201f09ddb0f8e101d
Rick <rrhvanhoof@gmail.com>
2024-02-07 15:47:29 +0100

    opdarcht 1

--- week1/opdracht1/src/Opdracht1.java
+++ week1/opdracht1/src/Opdracht1.java
@@ -1,7 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Opdracht1 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter output = new PrintWriter("temp.txt");
+
+        output.printf("amount is %f %e\r\n", 32.32, 32.32);
+        output.printf("amount is %5.4f %5.4e\r\n", 32.32, 32.32);
+        output.printf("%6b\r\n", (1>2));
+        output.printf("%6s\r\n", "Java");
+        output.close();
     }
 }
